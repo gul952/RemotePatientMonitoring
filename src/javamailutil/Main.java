db5919df050454217e6f760d740c6fa8b8d9f054
gul952 <gulwarinasaleem05@gmail.com>
2025-04-20 18:17:40 +0500

    Main.java

--- src/javamailutil/Main.java
+++ src/javamailutil/Main.java
@@ -2,6 +2,6 @@ package javamailutil;
 
 public class Main {
     public static void main(String args[]) throws Exception{
-        JavaMailUtil.sendMail("gulwarinasaleem05@gmail.com");
+        JavaMailUtil.sendMail("xxxx@gmail.com");
     }
 }
